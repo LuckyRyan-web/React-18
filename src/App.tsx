09b5d9a2df08ebdf6139e7b0fd389e974667c6dc
liuyuan <373005226@qq.com>
2022-04-10 23:53:57 +0800

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from 'react'
-import logo from './logo.svg'
-import style from './App.module.scss'
+// import style from './App.module.scss'
 import Index from '@/pages/Index'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
     return (
-        <div className={style.App}>
-            <header className={style.AppHeader}>
-                <img src={logo} className={style.AppLogo} alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className={style.AppLink}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-
-                <Index />
-            </header>
-        </div>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="" element={<Index />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -5,13 +5,30 @@
  */
 
 import React from 'react'
-//import classnames from 'classnames'
-//import style from './style.module.scss'
+// import classnames from 'classnames'
+import style from './style.module.scss'
+import logo from './logo.svg'
 
 interface IndexProps {}
 
 const Index: React.FC<IndexProps> = props => {
-    return <div>index.tsx</div>
+    return (
+        <div className={style.App}>
+            <header className={style.AppHeader}>
+                <img src={logo} className={style.AppLogo} alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className={style.AppLink}
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
 export default React.memo(Index)
